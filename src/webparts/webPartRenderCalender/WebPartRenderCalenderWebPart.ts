b5b938eddd77f6ccd3434ab934d49eb3154bbf3b
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-08-07 00:33:40 +0300

    'Fix-Bag'

--- src/webparts/webPartRenderCalender/WebPartRenderCalenderWebPart.ts
+++ src/webparts/webPartRenderCalender/WebPartRenderCalenderWebPart.ts
@@ -11,29 +11,14 @@ import * as strings from 'WebPartRenderCalenderWebPartStrings';
 import WebPartRenderCalender from './components/WebPartRenderCalender';
 import { IWebPartRenderCalenderProps } from './components/IWebPartRenderCalenderProps';
 
-import {Event} from "@microsoft/microsoft-graph-types";
 
 
 export interface IWebPartRenderCalenderWebPartProps {
     idCalendar: string;
     connectToggle: boolean;
-    dataEventsFromOtherWP: Event[];
 }
 
 export default class WebPartRenderCalenderWebPart extends BaseClientSideWebPart<IWebPartRenderCalenderWebPartProps> {
-
-    private dataEvents: Event[] = [];
-    //
-    // public onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any): void {
-    //     if (propertyPath === 'connectToggle') {
-    //         if (newValue === false){
-    //             Store.subscribe(dataEvent => {
-    //                 this.dataEvents = dataEvent;
-    //                 this.render();
-    //             });
-    //         }
-    //     }
-    // }
 
   public render(): void {
     const element: React.ReactElement<IWebPartRenderCalenderProps > = React.createElement(
@@ -41,7 +26,6 @@ export default class WebPartRenderCalenderWebPart extends BaseClientSideWebPart<
       {
           idCalendar: this.properties.idCalendar,
           connectToggle: this.properties.connectToggle,
-          dataEventsFromOtherWP: this.dataEvents,
           context: this.context
       }
     );
--- src/webparts/webPartRenderCalender/components/IWebPartRenderCalenderProps.ts
+++ src/webparts/webPartRenderCalender/components/IWebPartRenderCalenderProps.ts
@@ -1,9 +1,7 @@
 import { WebPartContext  } from "@microsoft/sp-webpart-base";
-import {Event} from "@microsoft/microsoft-graph-types";
 
 export interface IWebPartRenderCalenderProps {
     idCalendar: string;
     connectToggle: boolean;
     context: WebPartContext;
-    dataEventsFromOtherWP: Event[];
 }
